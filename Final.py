2ce0f03cd3c97783b0455b2a4156ae5d8a04f188
fbryant92 <forrestcbryant@gmail.com>
2022-05-19 17:13:30 -0700

    Finished structured english

---
+++ Final.py
@@ -0,0 +1,16 @@
+
+
+"""
+The program is meant to display number of grades.
+Also the number of grades that are above average.
+There will be two functions working in this program.
+Function1 will be to Kickstart the program and Function2 will be to calculate the percentage of grades that are above average.
+
+Function1 will Kickstart the application
+Function2 will output(Number of Grades,average grade, and percentage of grades above average)
+
+Number of Grades: 24
+Average Grade: 83.25
+Percentage of grades above average: 54.17%
+"""
+
